0a49673a999c84ef0cffe704aeec97e1ee7db8b8
David Akre <dakre21@gmail.com>
2018-01-24 19:44:48 -0700

    Adding spam filter to the hw repo

--- hw1/spamfilter.py
+++ hw1/spamfilter.py
@@ -7,10 +7,41 @@ Validation error based on the input spam filter data set.
 """
 
 import numpy as np
-from sklearn.model_selection import KFold
+import pandas as pd
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.naive_bayes import GaussianNB
+
+# Step 0: Forward declarations
+filepath = "data/spambase_train.csv"
+nf = 5
 
 # Step 1: Read in spamfilter training data
+dataset = pd.read_csv(filepath)
+X = dataset.values[:,:-1]
+y = dataset.values[:,-1]
+size = dataset.columns.size
 
-# Step 2: Create Naive Bayes Filter
+# Step 2: Create 5-Fold Validation Model and Report Error
+kf = KFold(n_splits=nf)
+kf = kf.split(X)
 
-# Step 3: Create 5-Fold Validation Model and Report Error
+# Step 3: Create Naive Bayes Filter (using GaussianNB library call from
+# sklearn)
+score = []
+for test, train in kf:
+    gnb = GaussianNB()
+    score.append(cross_val_score(gnb, X[test], y[test]))
+
+# Step 4: Compute validation output
+output = 0
+i = 0
+for si in score:
+    for sn in si:
+        output += sn
+        i += 1
+
+avg = output / i
+
+print "5-Fold Validation Error Output: %f" % (avg)
+
+
